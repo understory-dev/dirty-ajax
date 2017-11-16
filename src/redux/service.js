0edfa11ed1f12df423bb25e0cc86bc46a92f657c
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-11-16 13:52:36 -0600

    translate user from server to our version

--- src/redux/service.js
+++ src/redux/service.js
@@ -4,6 +4,14 @@ const simulateServerResponse = response => new Promise(resolve => {
   }, 500)
 })
 
+const translateServerUserToUser = (serverUser) => {
+  // This works OK, but it's still not really obvious to the rest of the
+  // application what a `user` looks like to us.
+  return {
+    email: serverUser.email,
+  }
+}
+
 
 export const loadInfo = () =>
   simulateServerResponse(
@@ -34,6 +42,7 @@ export const loadInfo = () =>
   ).then(resp => {
     // I don't want to return the raw server response.  I just want the "meat",
     // so I clean it up a bit here.
-
     return resp.user
   })
+  // The server has its idea of what a user is, but I don't need all that.
+  .then(translateServerUserToUser)
